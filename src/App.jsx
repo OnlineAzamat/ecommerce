f2130e713348dd3ea99f518b2e63b08efbb54de8
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-03-15 09:19:18 +0500

    new repo

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./components/Navbar"
+import "./styles.sass"
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
